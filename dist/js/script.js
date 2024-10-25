c32bc66f53796c199793d8c100bf84be115d2ed8
Ahmad Miftah Hidayat <hidayatahmadm@gmail.com>
2024-10-25 12:21:44 +0700

    Add files via upload

---
+++ dist/js/script.js
@@ -0,0 +1,9 @@
+// hamburger
+
+const hamburger = document.querySelector('#hamburger');
+const navmenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('hamburger-active');
+    navmenu.classList.toggle('hidden');
+});
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center:true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#006fff',
+        primary700: '#004EB3',
+        primary100: '#E6F1FF',
+
+        dark: '#00204a',
+
+        secondary: '#19ae5d',
+        secondary700: '#127A41',
+
+        tertiary: '#fdb44b',
+        tertiary700: '#B17E35',
+      },
+      fontFamily: {
+        jakarta: ["Plus Jakarta Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
